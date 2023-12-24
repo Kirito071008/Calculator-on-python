9c93ec3e953b113ad75161c3fcacadb333bca056
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2023-12-24 23:45:14 +0100

    Update Calculator.py
    Code optimization

--- Calculator.py
+++ Calculator.py
@@ -1,204 +1,79 @@
+import math
+from fractions import Fraction
 print("Welcome to calculator")
 print("Made by @Kirito071008")
-import math
-while True:
-    print("If you want to stop the calculator type (stop)")
-    e1 = input("Do you want to do:\n 1)math\n 2)physics?")
-    if e1 == "math" or e1 == "1":        
-         i1 = input("Do you want to do:\n 1)raise a number by a power\n 2)the square root of a number\n 3)the round of a number\n 4)how further is a number from zero\n 5) simple math function?")
-         if i1 == "raise a number by a power" or i1 == "1":
-          n1 = int(input("What's the number you want to raise? "))
-          p1 = int(input("What's the value of the power? "))
-          s1 = pow(n1,p1)
-          print(s1)
-         elif i1 == "the square root of a number" or i1 == "2":
-          n1 = int(input("Of what number? "))
-          s1 = (math.sqrt(n1))
-          print(s1)
-         elif i1 == "the round of a number" or i1 == "3":
-          n1 = float(input("What number do you want to round? "))
-          q1 = str(input("Do you want to round for excess or for defect? "))
-          if q1 == "excess" or q1 == "Excess":
-            s1 = (math.ceil(n1))
-            print(s1)
-          elif q1 == "Defect" or q1 == "defect":
-            s2 = (math.floor(n1))
-            print(s2)
-         elif i1 == "How further is a number from zero" or i1 == "4":
-            n1 = float(input("What's the number? "))
-            s1 = (abs(n1))
-            print(s1)
-         elif i1 == "simple math function" or i1 == "5":
-           q1 = input("Of how many numbers? (max 4) ")
-           if q1 == "2":
-            n1 = int(input("What's the first number? "))
-            n2 = input("What math operation do you want to use?")
-            n3 = int(input("What's the third second number?"))
-            if n2 == "+":
-                s1 = n1 + n3
-                print(s1)
-            if n2 == "-":
-                s1 = n1 - n3
-                print(s1)
-            if n2 == "*":
-                s1 = n1 * n3
-                print(s1)
-            if n2 == "/":
-                s1 = n1 / n3
-                print(s1)
-           elif q1 == "3":
-             n1 = int(input("What's the first number? "))
-             n2 = input("What math operation do you want to use?")
-             n3 = int(input("What's the second number? "))
-             n4 = input("What's the second math operation do you want to use?")
-             n5 = int(input("What's the third number?"))
-             if n2 == "+" and n4 == "+":
-                s1 = n1 + n3 + n4
-                print(s1)
-             if n2 == "+" and n4 == "-":
-                s1 = n1 + n3 - n5
-                print(s1)
-             if n2 == "+" and n4 == "*":
-                s1 = n1 + n3 * n5
-                print(s1)
-             if n2 == "+" and n2 == "/":
-                s1 = n1 + n3 / n5
-                print(s1)
-             if n2 == "-" and n4 == "+":
-                s1 = n1 - n3 + n5
-                print(s1)
-             if n2 == "-" and n4 == "-":
-                s1 = n1 - n3 - n5
-                print(s1)
-             if n2 == "-" and n4 == "*":
-                s1 = n1 - n3 * n5
-                print(s1)
-             if n2 == "-" and n4 == "/":
-                s1 = n1 - n3 / n5
-                print(s1)
-             if n2 == "*" and n4 == "-":
-                s1 = n1 * n3 - n5
-                print(s1)
-             if n2 == "*" and n4 == "+":
-                s1 = n1 * n3 + n5
-                print(s1)
-             if n2 == "*" and n4 == "*":
-                s1 = n1 * n3 * n5
-                print(s1)
-             if n2 == "*" and n4 == "/":
-                s1 = n1 * n3 / n5
-                print(s1)
-             if n2 == "/" and n4 == "-":
-                s1 = n1 / n3 - n5
-                print(s1)
-             if n2 == "/" and n4 == "+":
-                s1 = n1 / n3 + n5
-                print(s1)
-             if n2 == "/" and n4 == "*":
-                s1 = n1 / n3 * n5
-                print(s1)
-             if n2 == "/" and n4 == "/":
-                s1 = n1 / n3 / n5
-                print(s1)
-           elif q1 == "4":
-             print("Disclaimer, i have not yet write all the possibility, if you find one that didn't work please report to me and i will add it to the code")
-             n1 = int(input("What's the first number?"))
-             n2 = input("What math operation do you want to use?")
-             n3 = int(input("What's the second number?"))
-             n4 = input("What's the second math operation do you want to use?")
-             n5 = int(input("What's the third number?"))
-             n6 = input("What's the third math operationd do you want to use?")
-             n7 = int(input("What's the fourth number?"))
-             if n2 == "+" and n4 == "+" and n6 == "+":
-                s1 = n1 + n3 + n5 + n7
-                print(s1)
-             if n2 == "+" and n4 == "+" and n6 == "-":
-                s1 = n1 + n3 + n5 - n7
-                print(s1)
-             if n2 == "+" and n4 == "+" and n6 == "*":
-                s1 = n1 + n3 + n5 * n7
-                print(s1)
-             if n2 == "+" and n4 == "+" and n6 == "/":
-                s1 = n1 + n3 + n5 / n7
-                print(s1)
-             if n2 == "+" and n4 == "-" and n6 == "+":
-                s1 = n1 + n3 - n5 + n7
-                print(s1)
-             if n2 == "+" and n4 == "*" and n6 == "+":
-                s1 = n1 + n3 * n5 + n7
-                print(s1)
-             if n2 == "+" and n4 == "/" and n6 == "+":
-                s1 = n1 + n3 / n5 + n7
-                print(s1)
-             if n2 == "-" and n4 == "+" and n6 == "+":
-                s1 = n1 - n3 + n5 + n7
-                print(s1)
-             if n2 == "*" and n4 == "-" and n6 == "+":
-                s1 = n1 * n3 - n5 + n7
-                print(s1)
-             if n2 == "/" and n4 == "-" and n6 == "+":
-                s1 = n1 / n3 + n5 + n7
-                print(s1)
-             if n2 == "+" and n4 == "-" and n6 == "-":
-                s1 = n1 + n3 - n5 - n7
-                print(s1)
-             if n2 == "+" and n4 == "*" and n6 == "*":
-                s1 = n1 + n3 * n5 * n7
-                print(s1)
-             if n2 == "+" and n4 == "/" and n6 == "/":
-                s1 = n1 + n3 / n5 / n7
-                print(s1)
-             if n2 == "-" and n4 == "-" and n6 == "-":
-                s1 = n1 - n3 - n5 - n7
-                print(s1)
-             if n2 == "*" and n4 == "*" and n6 == "*":
-                s1 = n1 * n3 * n5 * n7
-                print(s1)
-             if n2 == "/" and n4 == "/" and n6 == "/":
-                s1 = n1 / n3 / n5 / n7
-                print(s1)
-             if n2 == "+" and n4 == "*" and n6 == "/":
-                s1 = n1 + n3 * n5 / n7
-                print(s1)
-             if n2 == "-" and n4 == "*" and n6 == "/":
-                s1 = n1 - n3 * n5 / n7
-                print(s1)
-             if n2 == "/" and n4 == "+" and n6 == "*":
-                s1 = n1 / n3 + n5 * n7
-                print(s1)
-             if n2 == "*" and n4 == "/" and n6 == "*":
-                s1 = n1 * n3 / n5 * n7
-                print(s1)
-             if n2 == "/" and n4 == "+" and n6 == "-":
-                s1 = n1 / n3 + n5 - n7
-                print(s1)
-             if n2 == "*" and n4 == "/" and n6 == "/":
-                s1 = n1 * n3 / n5 / n7
-                print(s1)
-             if n2 == "*" and n4 == "*" and n6 == "/":
-                s1 = n1 * n3 * n5 / n7
-                print(s1)
-             if n2 == "*" and n4 == "+" and n6 == "-":
-                s1 = n1 * n3 - n5 - n7
-                print(s1)
-             if n2 == "*" and n4 == "-" and n6 == "-":
-                s1 = n1 * n3 - n5 - n7
-                print(s1)
-            #combinations yet to be tested
-    elif e1 == "2":
-        i1 = input("Do you want to:\n 1)Convert\n 2) Use formulas")
-        if i1 == "Convert" or i1 == "convert" or i1 == "CONVERT" or i1 == "1":
-             q2 = input("1)convert km/h to m/s or 2)m/s to km/h?")
-             if q2 == "convert km/h to m/s" or q2 == "1":
-              n2 = float(input("What's the number you want to convert?"))
+def menu():
+    x = input("Do you want to do:\n1) Math\n2) Physics\n")
+    if x.lower() == "math" or x == "1":
+        mat()
+    elif x.lower() == "physics" or x == "2":
+        physics()
+    elif KeyboardInterrupt:
+        print("Bye!")
+    else:
+        print("Invalid input")
+def mat():
+    x = input("Do you want to do:\n1)1 Normal calculator\n2) Quadratic equations\n")
+    if x.lower() == "raise a number by a power" or x == "1":
+        while True:
+            try:
+                x = (input("Write your equation:\n"))
+                y = eval(x, {"__builtins__": None}, {"sqrt": math.sqrt})
+                print((y))
+            except (SyntaxError,ValueError,) as e:
+                print(f"Error: {e}")
+                menu()
+    elif x.lower() == "quadratic equations" or x == "2":
+        while True:
+              a1 = int(input("First number:\t"))
+              b1 = int(input("Second:\t"))
+              c1 = int(input("Third:\t"))
+              x = int("4")
+              y = int(b1)*int(b1)
+              s = int(x)*int(a1)*int(c1)
+              ss = int(y)-int(s)
+              if ss < int(0):
+                  print("Δ<0, sorry but i can't solve this :(")
+                  menu()
+                  break
+              elif ss == int(0):
+                       a = 2*int(a1)
+                       b = -1*b1
+                       print(f"X = {Fraction(int(b),int(a))}")
+                       if c.lower() == "yes" or c.lower() == "y":
+                            continue
+                       else:
+                            menu()
+                            break
+              elif ss > 0:
+                       a = 2*a1
+                       b = -b1+math.sqrt(ss)
+                       a2 = 2*a1
+                       b2 = -b1-math.sqrt(ss)
+                       print(f"X1 = {Fraction(int(b),int(a))}")
+                       print(f"X2 = {Fraction(int(b2),int(a2))}")
+                       c = input("Continue?")
+                       if c.lower() == "yes" or c.lower() == "y":
+                            continue
+                       else:
+                            menu()
+                            break
+def physics():
+        while True:
+            i1 = input("Do you want to:\n1) Convert\n2) Use formulas")
+            if i1.lower() == "convert" or i1 == "1":
+             q2 = input("Convert\n1) km/h to m/s\n2) m/s to km/h?\n")
+             if q2.lower() == "convert km/h to m/s" or q2 == "1":
+              n2 = float(input("What's the number you want to convert?:\t"))
               n3 = float("3.6")
               s1 = n2 / n3
               print(s1)
-             elif q2 == "m/s to km/h?" or q2 == "2":
-              n2 = float(input("What's the number you want to convert?"))
+             elif q2.lower() == "m/s to km/h?" or q2 == "2":
+              n2 = float(input("What's the number you want to convert?:\t"))
               n3 = float("3.6")
               s1 = n2 * n3
               print(s1)
-
-    elif e1 == "stop" or e1 == "Stop" or e1 == "STOP" or i1 == "stop" or i1 == "Stop" or i1 == "STOP":  
-        break
+            else:
+                menu()
+                break
+menu()
